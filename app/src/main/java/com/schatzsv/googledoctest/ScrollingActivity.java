92aa7549354491d2c995a8e2fc6ff688498d5e25
Steve Schatz <schatzsv@users.noreply.github.com>
2017-04-24 07:45:37 -0500

    Added Drive API and create app folder

--- app/src/main/java/com/schatzsv/googledoctest/ScrollingActivity.java
+++ app/src/main/java/com/schatzsv/googledoctest/ScrollingActivity.java
@@ -5,11 +5,29 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
-public class ScrollingActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.drive.Drive;
+import com.google.android.gms.drive.DriveApi;
+import com.google.android.gms.drive.DriveFolder;
+import com.google.android.gms.drive.Metadata;
+import com.google.android.gms.drive.MetadataChangeSet;
+import com.google.android.gms.drive.query.Filters;
+import com.google.android.gms.drive.query.Query;
+import com.google.android.gms.drive.query.SearchableField;
+
+public class ScrollingActivity extends AppCompatActivity
+        implements GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks {
+
+    GoogleApiClient mGoogleApiClient;
+    String TAG = "GoogleDocTest";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +40,82 @@ public class ScrollingActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                if (mGoogleApiClient.isConnected()) {
+                    Snackbar.make(view, "Connected", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                } else {
+                    Snackbar.make(view, "Not connected", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+
+                }
             }
         });
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Drive.API)
+                .addScope(Drive.SCOPE_FILE)
+                .addConnectionCallbacks(this)
+                .build();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        // An unresolvable error has occurred and a connection to Google APIs
+        // could not be established. Display an error message, or handle
+        // the failure silently
+        Log.d(TAG, "onConnectionFailed()");
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        Log.d(TAG, "onConnected()");
+        Query query = new Query.Builder()
+                .addFilter(Filters.and(Filters.eq(
+                        SearchableField.TITLE, "GoogleDocTest"),
+                        Filters.eq(SearchableField.TRASHED, false)))
+                .build();
+        Drive.DriveApi.query(mGoogleApiClient, query)
+                .setResultCallback(new ResultCallback<DriveApi.MetadataBufferResult>() {
+                    @Override
+                    public void onResult(DriveApi.MetadataBufferResult result) {
+                        if (!result.getStatus().isSuccess()) {
+                            Log.d(TAG, "Cannot create folder in the root.");
+                        } else {
+                            boolean isFound = false;
+                            for (Metadata m : result.getMetadataBuffer()) {
+                                if (m.getTitle().equals("GoogleDocTest")) {
+                                    Log.d(TAG, "Folder exists");
+                                    isFound = true;
+                                    break;
+                                }
+                            }
+                            if (!isFound) {
+                                Log.d(TAG, "Folder not found; creating it.");
+                                MetadataChangeSet changeSet = new MetadataChangeSet.Builder()
+                                        .setTitle("GoogleDocTest")
+                                        .build();
+                                Drive.DriveApi.getRootFolder(mGoogleApiClient)
+                                        .createFolder(mGoogleApiClient, changeSet)
+                                        .setResultCallback(new ResultCallback<DriveFolder.DriveFolderResult>() {
+                                            @Override
+                                            public void onResult(DriveFolder.DriveFolderResult result) {
+                                                if (!result.getStatus().isSuccess()) {
+                                                    Log.d(TAG, "Error while trying to create the folder");
+                                                } else {
+                                                    Log.d(TAG, "Created a folder");
+                                                }
+                                            }
+                                        });
+                            }
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+        Log.d(TAG, "onConnectionSuspended()");
     }
 
     @Override
@@ -44,6 +134,15 @@ public class ScrollingActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            return true;
+        }
+        if (id == R.id.action_checkconn) {
+            boolean rv = mGoogleApiClient.isConnected();
+            if (rv) {
+                Toast.makeText(this, "Connected", Toast.LENGTH_LONG).show();
+
+            }
+
             return true;
         }
         return super.onOptionsItemSelected(item);
